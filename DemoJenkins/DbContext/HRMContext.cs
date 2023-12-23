032d0058278c9f54d23f3d52d7274d4cdf9f8f78
chivy140820a <chivy1408@gmail.com>
2023-12-23 10:33:56 +0700

    update

---
+++ DemoJenkins/DbContext/HRMContext.cs
@@ -0,0 +1,15 @@
+﻿using DemoJenkins.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DemoJenkins.DbContext
+{
+    public class HRMContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public HRMContext(DbContextOptions<HRMContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
